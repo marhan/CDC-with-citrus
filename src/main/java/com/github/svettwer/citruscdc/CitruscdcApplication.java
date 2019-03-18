16fa18479e9949e3ba4668ecc7626b3762a3d80c
Sven Hettwer <sven.hettwer@consol.de>
2019-03-18 13:18:48 +0100

    Added basic project structure

---
+++ src/main/java/com/github/svettwer/citruscdc/CitruscdcApplication.java
@@ -0,0 +1,13 @@
+package com.github.svettwer.citruscdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitruscdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitruscdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/svettwer/citruscdc/CitruscdcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.svettwer.citruscdc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CitruscdcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
